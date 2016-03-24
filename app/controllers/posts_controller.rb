f4de3b66cef4452cc80af5e112c412775f85774f
martamoscicka1@gmail.com <martamoscicka1@gmail.com>
2016-03-24 18:31:16 +0100

    sorting posts fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   include  ActionView::Helpers::SanitizeHelper
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).all
   end
 
   def show
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
     end
 
     def strippedtags(content)
-      tags = %w(a acronym b strong i em li ul ol h1 h2 h3 h4 h5 h6 blockquote u br cite sub sup ins p)
+      tags = %w(a acronym b strong i em li ul ol h1 h2 h3 h4 h5 h6 blockquote u img br cite sub sup ins p)
       sanitize(content, tags: tags, attributes: %w(href title))
     end
 end
