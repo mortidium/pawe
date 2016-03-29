562a98cbb1a06abd690db1f089adf2e059e398c2
martamoscicka1@gmail.com <martamoscicka1@gmail.com>
2016-03-29 23:59:25 +0200

    general appearance and access changed

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,36 +1,46 @@
 class PartsController < ApplicationController
+
+    before_action :require_editor, only: [:new, :edit, :show, :index]
+    
   def index
     @parts = Part.all
   end
-    def new
+
+  def new
     @part = Part.new
   end
 
   def edit
     @part = Part.find(params[:id])
   end
+    
+    def destroy
+    @part = Part.find(params[:id])
+    @part.destroy
+    redirect_to(:action => 'index')
+  end
 
   def update
     @part =Part.find(params[:id])
     if @part.update_attributes(part_params)
-        redirect_to(:action => 'index', :id => @part.id)
+      redirect_to(:action => 'index', :id => @part.id)
     else
       render 'edit'
     end
   end
 
-    def create
-        @part = Part.new(part_params)
+  def create
+    @part = Part.new(part_params)
     if @part.save
-       redirect_to '/parts'
+      redirect_to '/parts'
     else
       render 'new'
     end
 
   end
-    
-    private
-    def part_params
-        params.require(:part).permit(:post,:tag)
-    end
+
+  private
+  def part_params
+    params.require(:part).permit(:post_id, :tag_id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,29 +1,29 @@
 class Post < ActiveRecord::Base
-  has_many :parts
-    has_many :tags, through: :parts
-    
-    def self.tagged_with(title)
-        Tag.find_by_name!(title).posts
+    has_many :parts, :dependent => :destroy
+  has_many :tags, through: :parts
+
+  def self.tagged_with(title)
+    Tag.find_by_name!(title).posts
   end
-    
+
   def self.tag_counts
     Tag.select("tags.*, count(parts.tag_id) as count").
-      joins(:parts).group("parts.tag_id")
+        joins(:parts).group("parts.tag_id")
   end
-  
+
   def tag_list
-      tags.map(&:title).join(", ")
+    tags.map(&:title).join(", ")
   end
-  
-    def tag_list=(titles)
-        self.tags = titles.split(",").map do |n|
-            Tag.where(title: n.strip).first_or_create!
+
+  def tag_list=(titles)
+    self.tags = titles.split(",").map do |n|
+      Tag.where(title: n.strip).first_or_create!
     end
   end
-    
+
   has_attached_file :image, default_url: "http://41.media.tumblr.com/573443f1697fde0832eeecd5f4996e40/tumblr_o3zphg6cy31symt9go1_500.jpg"
   validates :title, presence: true,
-            length: { minimum: 5 }
+            length: {minimum: 5}
   validates_attachment :image,
-  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+                       content_type: {content_type: ["image/jpeg", "image/gif", "image/png"]}
 end
