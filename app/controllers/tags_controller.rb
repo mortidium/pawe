5aa7948094e2efd6bc2554342297bd1c490061fb
martamoscicka1@gmail.com <martamoscicka1@gmail.com>
2016-04-04 16:36:09 +0200

    pagination of posts in tags show added

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,7 +6,7 @@ class TagsController < ApplicationController
 
   def show
     @tag=Tag.find(params[:id])
-    @posts = @tag.posts
+    @posts = @tag.posts.paginate(:page => params[:page], :per_page => 4)
   end
 
 
