8e197592c766669f1dcc9f81b761a36223422944
martamoscicka1@gmail.com <martamoscicka1@gmail.com>
2016-05-09 00:05:38 +0200

    likes visually enhanced

--- db/migrate/20160404211246_add_likes_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddLikesToPosts < ActiveRecord::Migration
-  def change
-      add_column :posts, :likes, :longint
-  end
-end
--- db/migrate/20160404211257_add_likes_to_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddLikesToTags < ActiveRecord::Migration
-  def change
-      add_column :tags, :likes, :longint
-  end
-end
