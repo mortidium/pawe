eef7f800057a52030811c246b13405f2925a5de7
martamoscicka1@gmail.com <martamoscicka1@gmail.com>
2016-05-08 23:19:08 +0200

    evs wrong

--- db/schema.rb
+++ db/schema.rb
@@ -48,26 +48,8 @@ ActiveRecord::Schema.define(version: 20160428101309) do
     t.string   "description"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.integer  "cached_votes_total",      default: 0
-    t.integer  "cached_votes_score",      default: 0
-    t.integer  "cached_votes_up",         default: 0
-    t.integer  "cached_votes_down",       default: 0
-    t.integer  "cached_weighted_score",   default: 0
-    t.integer  "cached_weighted_total",   default: 0
-    t.float    "cached_weighted_average", default: 0.0
+    
   end
-
-  add_index "posts", ["cached_votes_down"], name: "index_posts_on_cached_votes_down"
-  add_index "posts", ["cached_votes_score"], name: "index_posts_on_cached_votes_score"
-  add_index "posts", ["cached_votes_total"], name: "index_posts_on_cached_votes_total"
-  add_index "posts", ["cached_votes_up"], name: "index_posts_on_cached_votes_up"
-  add_index "posts", ["cached_weighted_average"], name: "index_posts_on_cached_weighted_average"
-  add_index "posts", ["cached_weighted_score"], name: "index_posts_on_cached_weighted_score"
-  add_index "posts", ["cached_weighted_total"], name: "index_posts_on_cached_weighted_total"
 
   create_table "tags", force: :cascade do |t|
     t.string   "title"
@@ -85,20 +67,5 @@ ActiveRecord::Schema.define(version: 20160428101309) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id"
-    t.string   "votable_type"
-    t.integer  "voter_id"
-    t.string   "voter_type"
-    t.boolean  "vote_flag"
-    t.string   "vote_scope"
-    t.integer  "vote_weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
